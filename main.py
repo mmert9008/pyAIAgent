404b9c929a3adafed1a7ea9de52f158e52e84844
mmert9008 <michael.mertens.jr@gmail.com>
2025-11-01 13:33:28 -0500

    added verbose functionality to submit for CH1 L6

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ from google import genai
 
 def main():
     if len(sys.argv) < 2:
-        print('Usage: uv run main.py "<your prompt>"')
+        print('Usage: uv run main.py "<your prompt>" [--verbose]')
         sys.exit(1)
 
     prompt = sys.argv[1]
+
+    verbose = "--verbose" in sys.argv
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
+
+    if verbose:
+        print(f"User prompt: {prompt}")
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
@@ -24,8 +29,9 @@ def main():
 
     print(response.text)
 
-    print(f"\nPrompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"\nPrompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
