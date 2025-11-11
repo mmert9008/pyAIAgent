37dddf9ba438a21d74f7b55c759a409065a53370
mmert9008 <michael.mertens.jr@gmail.com>
2025-11-11 13:10:03 -0600

    ready to submit CH3 L1

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import sys
-
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
@@ -8,17 +7,19 @@ from google.genai import types
 
 def main():
     if len(sys.argv) < 2:
-        print('Usage: uv run main.py "<your prompt>" [--verbose]')
+        print("Usage: uv run main.py \"<your prompt>\" [--verbose]")
         sys.exit(1)
 
     prompt = sys.argv[1]
 
     verbose = "--verbose" in sys.argv
-
+    
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
+
+    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
 
     if verbose:
         print(f"User prompt: {prompt}")
@@ -26,10 +27,11 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
-
+    
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
 
     print(response.text)
