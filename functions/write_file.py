0ab4f97595304c8537708760a12a0b1f6869cd44
mmert9008 <michael.mertens.jr@gmail.com>
2025-11-16 15:57:24 -0600

    ready to submit for CH3 L3

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,5 +1,7 @@
 import os
 
+from google.genai import types
+
 
 def write_file(working_directory, file_path, content):
     try:
@@ -24,3 +26,23 @@ def write_file(working_directory, file_path, content):
 
     except Exception as e:
         return f"Error: {str(e)}"
+
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes or overwrites content to a file, constrained to the working directory. Creates the file and parent directories if they don't exist.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to write, relative to the working directory.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to write to the file.",
+            ),
+        },
+        required=["file_path", "content"],
+    ),
+)
